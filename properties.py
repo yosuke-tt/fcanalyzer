3aecd27137038fa7daa0e1fa6c87933db745b276
cellcPc <cellcPc@email>
2022-12-11 00:40:44 +0900

    張力の関数を追加

--- properties.py
+++ properties.py
@@ -1,5 +1,10 @@
+import numpy as np
+
+
 from afm import AFM
 
+
+T_DASH=1/50000
 
 def hertz_constant(afm:AFM):
     poission = 0.5
@@ -8,3 +13,11 @@ def hertz_constant(afm:AFM):
 def hertz_apperant(fc, hertz_young):
     return hertz_constant(fc.afm)*hertz_young*fc.indentation**(3/2)
 
+def et_e0(t, e0, einf, alpha):
+    return einf+(e0-einf)*(1+t/T_DASH)**(-alpha)
+
+def et_e1(t, e1, einf, alpha):
+    return einf+(e1-einf)*(t+T_DASH)**(-alpha)
+    
+def tension(fc, tension):
+    return np.pi*tension*fc.indentation

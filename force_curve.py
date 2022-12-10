b197254df60b3418ad121152cfa7235db28441a9
cellcPc <cellcPc@email>
2022-12-10 13:04:40 +0900

    force_curve_abcいらないかもな

--- force_curve.py
+++ force_curve.py
@@ -1,3 +1,10 @@
 import numpy as np
 
 import matplotlib.pyplot as plt
+
+from force_curve_abc import ForceCurveAbstract
+from dataclasses import dataclass
+
+@dataclass
+class ForceCurve(ForceCurveAbstract):
+    pass

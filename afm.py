bf490b75830264df30da60351adb135d5ff5ea51
cellcPc <cellcPc@email>
2022-12-10 12:33:30 +0900

    ピエゾについての情報を追加しました。

--- afm.py
+++ afm.py
@@ -6,3 +6,8 @@ class AFM:
     invols:float
     k:float
     no_afm:int=None
+    um_per_v_dict:dict[int,float] = {2:25,3:25}
+    def __post_init__(self):
+        
+        self.um_per_v:float = self.um_per_v_dict[self.no_afm]
+        
